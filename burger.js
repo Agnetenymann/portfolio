e0e3a24c341964f9b6fc0620a18f9c1f3b4adb8e
Agnete Nymann <ag.nymann@gmail.com>
2025-01-06 01:59:29 +0100

    crazy

--- burger.js
+++ burger.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
 const menu = document.querySelector(".menu");
+const menuHover = document.querySelector(".menu_hover");
 
 burger.addEventListener("click", burgerClick);
 function burgerClick() {
@@ -12,3 +13,20 @@ function menuClick() {
   burger.classList.remove("active");
   nav.classList.remove("active");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  burger.addEventListener("click", () => {
+    // Skift menuens synlighed
+
+    menu.classList.toggle("open");
+
+    // Skift farven og teksten på menu_hover
+    menuHover.classList.toggle("active");
+
+    if (menu.classList.contains("open")) {
+      menuHover.textContent = "[LUK]"; // Ændr teksten til [LUK]
+    } else {
+      menuHover.textContent = "[MENU]"; // Ændr teksten tilbage til [MENU]
+    }
+  });
+});
